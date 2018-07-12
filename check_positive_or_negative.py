87e83d289fc2ed03c9ffbf2034220a2363c7aef5
Ezhilprabu <41141076+Ezhilprabu@users.noreply.github.com>
2018-07-12 14:22:59 +0530

    Update check_positive_or_negative.py

--- check_positive_or_negative.py
+++ check_positive_or_negative.py
@@ -1,9 +1,9 @@
 number=int("input(""))
-	   if(number>=1 and number<=100000):
-	   	print("Positive")
-	   elif(number==0):
-	   	print("Zero")
-	   elif(number<0):
-	   	print("Negative")
-	   else:
-	   	print("Not in Range")
+if(number>=1 and number<=100000):
+	print("Positive")
+elif(number==0):
+	print("Zero")
+elif(number<0):
+	print("Negative")
+else:
+	print("Not in Range")
